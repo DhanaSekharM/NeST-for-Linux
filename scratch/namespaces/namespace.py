70901db1d90f10fd2c8538d84b82d61a39f77cd5
Shanthanu S Rai <shanthanu.s.rai9@gmail.com>
2019-09-26 23:33:05 +0530

    Added comments describing major steps

--- scratch/namespaces/namespace.py
+++ scratch/namespaces/namespace.py
@@ -1,3 +1,7 @@
+##########################################
+# Note: This script should be run as root
+##########################################
+
 import os
 import subprocess
 import argparse
@@ -11,25 +15,28 @@ def exec_subprocess(cmd, block = True):
 	else:
 		pass
 	
+# Default Network Namespaces
 n1 = "n1"
 n2 = "n2"
+
+# Parse Arguements
 cmdParser = argparse.ArgumentParser()
 cmdParser.add_argument('names', type=str, nargs=2)
 args = cmdParser.parse_args()
 n1 = args.names[0]
 n2 = args.names[1]
+
+# Add Network Namespace
+# TODO: Check if the namespace already exists
 exec_subprocess("ip netns add " + n1)
 exec_subprocess("ip netns add " + n2)
 
+# Setup virtual ethernet devices (veth)
 exec_subprocess("ip link add eth-n1 type veth peer name eth-n2")
-
-print('j')
-
 exec_subprocess("ip link set eth-n1 netns " + n1)
 exec_subprocess("ip link set eth-n2 netns " + n2)
 
-print('i')
-
+# Configure the ethernet devices
 exec_subprocess("ip netns exec " + n1 + " ip link set dev lo up")
 exec_subprocess("ip netns exec " + n1 + " ip link set dev eth-n1 up")
 exec_subprocess("ip netns exec " + n1 + " ip address add 10.0.0.1/24 dev eth-n1")
@@ -38,6 +45,7 @@ exec_subprocess("ip netns exec " + n2 + " ip link set dev lo up")
 exec_subprocess("ip netns exec " + n2 + " ip link set dev eth-n2 up")
 exec_subprocess("ip netns exec " + n2 + " ip address add 10.0.0.2/24 dev eth-n2")
 
+# Run server and client application
 t1 = threading.Thread(target=exec_subprocess, args=("ip netns exec " + n2 + " python3 server.py", )) 
 t2 = threading.Thread(target=exec_subprocess, args=("ip netns exec " + n1 + " telnet 10.0.0.2 1025", ))
 
@@ -52,7 +60,6 @@ t2.join()
 # exec_subprocess("ip netns exec " + n1 + " telnet 10.0.0.2 1025")
 # exec_subprocess("ip netns exec " + n1 + " python3 client.py", block=False)
 
+# Delete created Network Namespaces
 exec_subprocess("ip netns del " + n1)
 exec_subprocess("ip netns del " + n2)
-
-
